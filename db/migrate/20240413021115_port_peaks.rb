a9537cc884f6896c138f2049b837eda5b8a92531
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2024-04-14 23:01:06 -0400

    peaks migration fix

--- db/migrate/20240413021115_port_peaks.rb
+++ db/migrate/20240413021115_port_peaks.rb
@@ -3,7 +3,8 @@ class PortPeaks < ActiveRecord::Migration[7.0]
 
     Track.all.find_each do |track|
 
-      track.build_track_peak(data: track.metadata["peaks"])
+      peaks = track.metadata["peaks"] rescue []
+      track.build_track_peak(data: peaks)
       track.update(peaks: [])
     end
   end
