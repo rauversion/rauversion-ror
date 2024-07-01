c239de786ffb250804feb7667562eef740004eaf
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2024-06-30 22:22:44 -0400

    ui tweaks

--- app/helpers/tailwind_form_builder.rb
+++ app/helpers/tailwind_form_builder.rb
@@ -95,7 +95,7 @@ class TailwindFormBuilder < ActionView::Helpers::FormBuilder
   def text_area(attribute, options = {})
     @template.tag.div(class: "w-full sm:w-full py-2") do
       @template.label_tag(tr(options[:label] || attribute), nil) +
-        super(attribute, options.reverse_merge(class: "block w-full rounded-md border-0 py-1.5 dark:bg-gray-900 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brand-600 sm:text-sm sm:leading-6")) +
+        super(attribute, options.reverse_merge(class: "block w-full rounded-md border-0 py-1.5 bg-muted text-gray-900 shadow-sm ring-1 ring-inset ring-subtle placeholder:text-subtle focus:ring-2 focus:ring-inset focus:ring-brand-600 sm:text-sm sm:leading-6")) +
         field_details(attribute, object, options)
     end
   end
