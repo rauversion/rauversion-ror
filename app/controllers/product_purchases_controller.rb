f54e8a630e12aa6ab39667f0de75524722772dcf
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2024-10-28 00:56:49 -0300

    paginate orders

--- app/controllers/product_purchases_controller.rb
+++ app/controllers/product_purchases_controller.rb
@@ -2,7 +2,9 @@ class ProductPurchasesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @purchases = current_user.product_purchases.order(created_at: :desc)
+    @purchases = current_user.product_purchases
+    .order(created_at: :desc)
+    .page(params[:page]).per(30)
   end
 
   def show
